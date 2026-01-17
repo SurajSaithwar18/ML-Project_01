f38c3fcc0c7ab5862f1a463b1544ec4f7e612299
SurajSaithwar <saithwarsuraj18@gmail.com>
2026-01-17 23:54:53 +0530

    Simple Learning

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,4 @@
 import sys 
-import logging
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
@@ -13,6 +12,7 @@ class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
+        
     def __str__(self):
         return self.error_message
     
